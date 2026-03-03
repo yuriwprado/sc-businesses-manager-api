0df95b11bd930ebbe6d259809994a9da29283a03
yuriwprado <yuriwprado@gmail.com>
2026-03-03 20:04:46 -0300

    Project init

---
+++ src/main/java/com/yprado/sc_businesses_manager_api/ScBusinessesManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.yprado.sc_businesses_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScBusinessesManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScBusinessesManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yprado/sc_businesses_manager_api/ScBusinessesManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yprado.sc_businesses_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScBusinessesManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
